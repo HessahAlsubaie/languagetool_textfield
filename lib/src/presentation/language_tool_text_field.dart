ad02a8ecf1b27f86a7b02a482945d7e57c43f1bd
HessahAlsubaie <Alsubaiehessa9@gmail.com>
2024-03-11 11:55:03 +0300

    add a validator to the text field

--- lib/src/presentation/language_tool_text_field.dart
+++ lib/src/presentation/language_tool_text_field.dart
@@ -1,45 +1,159 @@
+// import 'package:flutter/material.dart';
+// import 'package:languagetool_textfield/src/core/controllers/language_tool_controller.dart';
+// import 'package:languagetool_textfield/src/utils/mistake_popup.dart';
+// import 'package:languagetool_textfield/src/utils/popup_overlay_renderer.dart';
+
+// /// A TextField widget that checks the grammar using the given
+// /// [LanguageToolController]
+// class LanguageToolTextField extends StatefulWidget {
+//   /// A style to use for the text being edited.
+//   final TextStyle? style;
+
+//   /// A decoration of this [TextField].
+//   final InputDecoration decoration;
+
+//   /// Color scheme to highlight mistakes
+//   final LanguageToolController controller;
+
+//   /// Mistake popup window
+//   final MistakePopup? mistakePopup;
+
+//   /// The maximum number of lines to show at one time, wrapping if necessary.
+//   final int? maxLines;
+
+//   /// The minimum number of lines to occupy when the content spans fewer lines.
+//   final int? minLines;
+
+//   /// Whether this widget's height will be sized to fill its parent.
+//   final bool expands;
+
+//   /// A language code like en-US, de-DE, fr, or auto to guess
+//   /// the language automatically.
+//   /// ```language``` = 'auto' by default.
+//   final String language;
+
+//   /// Determine text alignment
+//   /// textAlign = [TextAlign.start] by default.
+//   final TextAlign textAlign;
+
+//   /// Determine text Direction
+//   final TextDirection? textDirection;
+//   final String? Function(String?)? validator;
+
+//   /// Creates a widget that checks grammar errors.
+//   const LanguageToolTextField({
+//     required this.controller,
+//     this.style,
+//     this.decoration = const InputDecoration(),
+//     this.language = 'auto',
+//     this.mistakePopup,
+//     this.maxLines = 1,
+//     this.minLines,
+//     this.expands = false,
+//     this.textAlign = TextAlign.start,
+//     this.textDirection,
+//     this.validator,
+//     super.key,
+//   });
+
+//   @override
+//   State<LanguageToolTextField> createState() => _LanguageToolTextFieldState();
+// }
+
+// class _LanguageToolTextFieldState extends State<LanguageToolTextField> {
+//   static const _padding = 24.0;
+
+//   final _focusNode = FocusNode();
+//   final _scrollController = ScrollController();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     final controller = widget.controller;
+
+//     controller.focusNode = _focusNode;
+//     controller.language = widget.language;
+//     final defaultPopup = MistakePopup(popupRenderer: PopupOverlayRenderer());
+//     controller.popupWidget = widget.mistakePopup ?? defaultPopup;
+
+//     controller.addListener(_textControllerListener);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Form(
+//       key: _formKey,
+//       child: ListenableBuilder(
+//         listenable: widget.controller,
+//         builder: (_, __) {
+//           final fetchError = widget.controller.fetchError;
+      
+//           // it would probably look much better if the error would be shown on a
+//           // dedicated panel with field options
+//           final httpErrorText = Text(
+//             '$fetchError',
+//             style: TextStyle(
+//               color: widget.controller.highlightStyle.misspellingMistakeColor,
+//             ),
+//           );
+      
+//           final inputDecoration = widget.decoration.copyWith(
+//             suffix: fetchError != null ? httpErrorText : null,
+//           );
+      
+//           return Padding(
+//             padding: const EdgeInsets.all(_padding),
+//             child: Center(
+//               child: TextField(
+//                 textAlign: widget.textAlign,
+//                 textDirection: widget.textDirection,
+//                 focusNode: _focusNode,
+//                 controller: widget.controller,
+//                 scrollController: _scrollController,
+//                 decoration: inputDecoration,
+//                 minLines: widget.minLines,
+//                 maxLines: widget.maxLines,
+//                 expands: widget.expands,
+//                 style: widget.style,
+//                 validator: widget.validator,
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+
+//   void _textControllerListener() =>
+//       widget.controller.scrollOffset = _scrollController.offset;
+
+//   @override
+//   void dispose() {
+//     _focusNode.dispose();
+//     _scrollController.dispose();
+//     super.dispose();
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:languagetool_textfield/src/core/controllers/language_tool_controller.dart';
 import 'package:languagetool_textfield/src/utils/mistake_popup.dart';
 import 'package:languagetool_textfield/src/utils/popup_overlay_renderer.dart';
 
-/// A TextField widget that checks the grammar using the given
-/// [LanguageToolController]
 class LanguageToolTextField extends StatefulWidget {
-  /// A style to use for the text being edited.
   final TextStyle? style;
-
-  /// A decoration of this [TextField].
   final InputDecoration decoration;
-
-  /// Color scheme to highlight mistakes
   final LanguageToolController controller;
-
-  /// Mistake popup window
   final MistakePopup? mistakePopup;
-
-  /// The maximum number of lines to show at one time, wrapping if necessary.
   final int? maxLines;
-
-  /// The minimum number of lines to occupy when the content spans fewer lines.
   final int? minLines;
-
-  /// Whether this widget's height will be sized to fill its parent.
   final bool expands;
-
-  /// A language code like en-US, de-DE, fr, or auto to guess
-  /// the language automatically.
-  /// ```language``` = 'auto' by default.
   final String language;
-
-  /// Determine text alignment
-  /// textAlign = [TextAlign.start] by default.
   final TextAlign textAlign;
-
-  /// Determine text Direction
   final TextDirection? textDirection;
+  final String? Function(String?)? validator;
 
-  /// Creates a widget that checks grammar errors.
   const LanguageToolTextField({
     required this.controller,
     this.style,
@@ -51,16 +165,17 @@ class LanguageToolTextField extends StatefulWidget {
     this.expands = false,
     this.textAlign = TextAlign.start,
     this.textDirection,
-    super.key,
-  });
+    this.validator,
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<LanguageToolTextField> createState() => _LanguageToolTextFieldState();
+  _LanguageToolTextFieldState createState() => _LanguageToolTextFieldState();
 }
 
 class _LanguageToolTextFieldState extends State<LanguageToolTextField> {
   static const _padding = 24.0;
-
+  final _formKey = GlobalKey<FormState>();
   final _focusNode = FocusNode();
   final _scrollController = ScrollController();
 
@@ -79,42 +194,44 @@ class _LanguageToolTextFieldState extends State<LanguageToolTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: widget.controller,
-      builder: (_, __) {
-        final fetchError = widget.controller.fetchError;
+    return Form(
+      key: _formKey,
+      child: ListenableBuilder(
+        listenable: widget.controller,
+        builder: (_, __) {
+          final fetchError = widget.controller.fetchError;
 
-        // it would probably look much better if the error would be shown on a
-        // dedicated panel with field options
-        final httpErrorText = Text(
-          '$fetchError',
-          style: TextStyle(
-            color: widget.controller.highlightStyle.misspellingMistakeColor,
-          ),
-        );
-
-        final inputDecoration = widget.decoration.copyWith(
-          suffix: fetchError != null ? httpErrorText : null,
-        );
-
-        return Padding(
-          padding: const EdgeInsets.all(_padding),
-          child: Center(
-            child: TextField(
-              textAlign: widget.textAlign,
-              textDirection: widget.textDirection,
-              focusNode: _focusNode,
-              controller: widget.controller,
-              scrollController: _scrollController,
-              decoration: inputDecoration,
-              minLines: widget.minLines,
-              maxLines: widget.maxLines,
-              expands: widget.expands,
-              style: widget.style,
+          final httpErrorText = Text(
+            '$fetchError',
+            style: TextStyle(
+              color: widget.controller.highlightStyle.misspellingMistakeColor,
             ),
-          ),
-        );
-      },
+          );
+
+          final inputDecoration = widget.decoration.copyWith(
+            suffix: fetchError != null ? httpErrorText : null,
+          );
+
+          return Padding(
+            padding: const EdgeInsets.all(_padding),
+            child: Center(
+              child: TextFormField(
+                textAlign: widget.textAlign,
+                textDirection: widget.textDirection,
+                focusNode: _focusNode,
+                controller: widget.controller,
+                scrollController: _scrollController,
+                decoration: inputDecoration,
+                minLines: widget.minLines,
+                maxLines: widget.maxLines,
+                expands: widget.expands,
+                style: widget.style,
+                validator: widget.validator,
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 
